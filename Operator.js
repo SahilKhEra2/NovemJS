01653872844704bd0c0b5ce48476ee1eb05e8361
Sahil khera <146655290+SahilKhEra2@users.noreply.github.com>
2024-05-24 14:05:40 +0530

    Add files via upload

---
+++ Operator.js
@@ -0,0 +1,39 @@
+// operators
+/*
+arithmetic operators
+sum = +
+diff = -
+dividision = /
+remainder = %
+multiplication = * 
+*/
+
+
+// let res=6+(5-(8/5))*8
+
+
+// let num=5;
+
+// //psot increment
+// console.log(num++) // will give 5 
+// console.log(num) // will give 6
+// // pre increment
+// console.log(++num) // will give 6
+// console.log(num) // will give 6
+
+// num+=2;
+// num = num + 2;
+// console.log(num);
+
+// turnery operator 
+let age=20;
+if(age>=20){
+console.log('you are eligible to vote');
+}else{
+    console.log("you are no ");
+}
+
+age>=18?console.log("you are eligible to vote"):console.log("you are not");
+// age>=18 is condition 
+// ? this part denotes the output which will executed if condtion is true
+// : this is else party which will be executed if conditin is false  
